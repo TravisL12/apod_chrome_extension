ed06df7bd85065e62e25c5592a20e4d715dcfc13
Travis Lawrence <tlawrence@tumblr.com>
2017-09-14 10:51:20 -0700

    Wheel is balanced

--- public/js/DateWheel.js
+++ public/js/DateWheel.js
@@ -5,16 +5,24 @@ class DateWheel {
 
         this.days = [];
         for (let day = 1; day <= nums; day++) {
-            this.days.push(`<div class='date-val'><div class='num'>${day}</div></div>`);
+            this.days.push(`
+                <div class='date-val'>
+                    <div class='num'>${day}</div>
+                </div>`
+            );
         }
         this.el.innerHTML = this.days.join('');
-        this.rotation = 0;
+        let currentRotationDeg = 92;
+        this.setRotationAngle(0);
 
         this.el.addEventListener('mousewheel', function (e) {
-            this.rotation += e.deltaY * 0.1;
-            this.el.style.transform = `rotate3d(0,0,1,${this.rotation}deg)`; 
+            currentRotationDeg += e.deltaY * 0.1;
+            this.setRotationAngle(currentRotationDeg);
         }.bind(this));
+    }
 
+    setRotationAngle (angle) {
+        this.el.style.transform = `rotate3d(0,0,1,${angle}deg)`; 
     }
 
 }
