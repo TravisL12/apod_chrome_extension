e1286e1773b2bdd7e2488073df11f9b3ebbc21e2
Travis Lawrence <tlawrence@tumblr.com>
2018-04-26 14:33:54 -0700

    Ok GA is working again

--- client/scripts/utils/ga.js
+++ client/scripts/utils/ga.js
@@ -1,24 +1,17 @@
-import reqwest from 'reqwest';
-
 const GA_TRACKING_ID = 'UA-91390132-1';
 
 export default function(event) {
-  const data = {
-    v: 1,
-    tid: GA_TRACKING_ID,
-    t: 'pageview',
-    ec: event.category,
-    ea: event.action,
-    el: event.label,
-    dl: 'chrome-extension://aedpginojmhafbemcoelnppdcmlfjcdj/index.html',
-    dp: 'apod_trav.html',
-    ul: 'en-us',
-    dt: 'APOD by The Trav'
-  };
+  const xhr = new XMLHttpRequest();
+  const url = "http://www.google-analytics.com/collect";
+  const params = "v=1";
+  params += `&tid=${GA_TRACKING_ID}`;
+  params += `&cid=${'55555'}`;
+  params += `&t=${"event"}`;
+  params += `&ec=${event.category}`;
+  params += `&ea=${event.action}`;
+  params += `&el=${event.label}`;
+  params += `&z=${(1000000000 + Math.floor(Math.random() * (2147483647 - 1000000000)))}`;
 
-  reqwest({
-    method: 'POST',
-    url: 'https://www.google-analytics.com/collect',
-    data: data,
-  });
+  xhr.open("POST", url, true);
+  xhr.send(params);
 }
