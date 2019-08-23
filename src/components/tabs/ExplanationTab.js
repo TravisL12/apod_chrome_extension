198f532f05dfd5aa8c74f028ee205e5ee52efe3e
Travis Lawrence <travis.lawrence@uber.com>
2019-08-23 10:22:03 -0700

    Options page css working

--- src/components/tabs/ExplanationTab.js
+++ src/components/tabs/ExplanationTab.js
@@ -11,13 +11,17 @@ function ExplanationTab({
       <div className="explanation">{explanation}</div>
 
       <div className="external-links">
-        <a href={apodSourceLink(date)} target="_blank">
+        <a
+          href={apodSourceLink(date)}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           View this APOD
         </a>
-        <a href={hdurl} target="_blank">
+        <a href={hdurl} target="_blank" rel="noopener noreferrer">
           Hi-res
         </a>
-        <a href={url} target="_blank">
+        <a href={url} target="_blank" rel="noopener noreferrer">
           Low-res
         </a>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// import * as serviceWorker from "./serviceWorker";
 import Apod from "./components/Apod";
 import ga from "./utilities/ga";
 
@@ -20,8 +21,7 @@ chrome.storage.sync.get(
   }
 );
 
-// import * as serviceWorker from './serviceWorker';
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// serviceWorker.register();
