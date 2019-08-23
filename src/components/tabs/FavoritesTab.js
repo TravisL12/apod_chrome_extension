9f0462c1de0f53cda0e2d19bce440b2354da2717
Travis Lawrence <travis.lawrence@uber.com>
2019-08-23 12:43:07 -0700

    Favorites tab

--- src/components/tabs/FavoritesTab.js
+++ src/components/tabs/FavoritesTab.js
@@ -1,10 +1,50 @@
-import React from "react";
+/*global chrome*/
+import React, { useState, useEffect } from "react";
+import { isEmpty, keys } from "lodash";
+import { prettyDateFormat } from "../../DateManager";
 
 function FavoritesTab() {
+  const [favorites, setFavorites] = useState({});
+
+  useEffect(() => {
+    chrome.storage.sync.get(["apodFavorites"], favorites => {
+      setFavorites(favorites.apodFavorites);
+    });
+  }, []);
+
   return (
     <div class="favorites">
       <h2>Favorite APOD's</h2>
-      <ul id="drawer-list" />
+      {isEmpty(favorites) ? (
+        <li class="no-favorites">
+          <h4>You don't have any favorites yet!</h4>
+          <h4>
+            Click the "Save Favorite" button at the top right of the page!
+          </h4>
+        </li>
+      ) : (
+        <ul id="drawer-list">
+          {keys(favorites).map((date, idx) => {
+            const favorite = favorites[date];
+
+            return (
+              <li class="favorite" key={idx}>
+                <div class="favorite__image">
+                  <div
+                    class="favorite__image-image"
+                    style={{ backgroundImage: `url(${favorite.imgUrl})` }}
+                  />
+                </div>
+                <div class="favorite__title">
+                  <p class="favorite__title-date">{prettyDateFormat(date)}</p>
+                  <p class="favorite__title-title">{favorite.title}</p>
+                </div>
+                <div class="remove-favorite">Remove</div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
