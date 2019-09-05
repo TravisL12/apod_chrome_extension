87a846e3e5e6e8e2309d52abfb41efab7c17b079
Travis Lawrence <travis.lawrence@uber.com>
2019-09-05 10:45:46 -0700

    Enable new option, showTopSites

--- public/options.js
+++ public/options.js
@@ -4,43 +4,73 @@ class ApodOptions {
     this.restoreOptions();
   }
 
-  save(obj) {
+  save = obj => {
     chrome.storage.sync.set(obj);
     setTimeout(window.close, 350);
-  }
+  };
 
-  saveApodType() {
+  setDefaultValues = ({ apodType, hiResOnly, showTopSites }) => {
+    if (!apodType) {
+      chrome.storage.sync.set({
+        apodType: "random"
+      });
+    }
+    if (!hiResOnly) {
+      chrome.storage.sync.set({
+        hiResOnly: false
+      });
+    }
+    if (!showTopSites) {
+      chrome.storage.sync.set({
+        showTopSites: true
+      });
+    }
+  };
+
+  saveApodType = () => {
     this.save({ apodType: this.form["choose-apod"].value });
-  }
+  };
 
-  saveHiResOnly() {
+  saveHiResOnly = () => {
     this.save({ hiResOnly: this.form["high-res-only"].checked });
-  }
+  };
+
+  saveTopSitesToggle = () => {
+    this.save({ showTopSites: this.form["show-top-sites"].checked });
+  };
 
   restoreOptions() {
-    chrome.storage.sync.get(["apodType", "hiResOnly"], items => {
-      let type = items.apodType;
-      if (!type) {
-        type = "random";
-        chrome.storage.sync.set({
-          apodType: type
-        });
+    chrome.storage.sync.get(
+      ["apodType", "hiResOnly", "showTopSites"],
+      items => {
+        const { apodType, hiResOnly, showTopSites } = items;
+        this.setDefaultValues(items);
+
+        this.form[apodType].checked = true;
+        this.form["high-res-only"].checked = hiResOnly;
+        this.form["show-top-sites"].checked = showTopSites;
+
+        this.form["choose-apod"][0].addEventListener(
+          "change",
+          this.saveApodType
+        );
+
+        this.form["choose-apod"][1].addEventListener(
+          "change",
+          this.saveApodType
+        );
+
+        this.form["high-res-only"].addEventListener(
+          "change",
+          this.saveHiResOnly
+        );
+
+        this.form["show-top-sites"].addEventListener(
+          "change",
+          this.saveTopSitesToggle
+        );
       }
-      this.form[type].checked = true;
-      this.form["high-res-only"].checked = items.hiResOnly;
-      this.form["choose-apod"][0].addEventListener(
-        "change",
-        this.saveApodType.bind(this)
-      );
-      this.form["choose-apod"][1].addEventListener(
-        "change",
-        this.saveApodType.bind(this)
-      );
-      this.form["high-res-only"].addEventListener(
-        "change",
-        this.saveHiResOnly.bind(this)
-      );
-    });
+    );
   }
 }
 
