58fd2d7ddf83715f04964b0c1c428f79a8055cc7
Travis Lawrence <travis.lawrence@uber.com>
2020-01-24 13:12:54 -0800

    Get date check tests working

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,6 +2,8 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "../../index";
+import Apod from "../Apod";
+import { actualDate } from "../../utilities/dateUtility";
 
 const props = {
   apodType: "today",
@@ -14,9 +16,23 @@ const props = {
   isTodayLimitOn: false
 };
 
+const mockDateString = "2020-01-23";
+const mockedDate = actualDate(mockDateString);
+global.Date = jest.fn(() => mockedDate);
+
 describe("App Component", () => {
   it("renders component", () => {
     const component = shallow(<App {...props} />);
     expect(component).toHaveLength(1);
+  });
+
+  it("Does not render Apod if isLoading", () => {
+    const component = shallow(<App {...props} currentDate={"2020-01-10"} />);
+    expect(component.find(Apod)).toHaveLength(0);
+  });
+
+  it("Renders Apod if isLoading", () => {
+    const component = shallow(<App {...props} currentDate={mockDateString} />);
+    expect(component.find(Apod)).toHaveLength(1);
   });
 });
