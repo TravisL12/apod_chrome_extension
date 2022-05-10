56bdc78599458ab560288030a5aa417bd47bfcdb
Travis Lawrence <travis.lawrence12@gmail.com>
2022-05-10 13:58:18 -0700

    styling

--- src/pages/Newtab/Loading.tsx
+++ src/pages/Newtab/Loading.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // @ts-expect-error
 import sunLogo from '../../assets/img/sun_loader.gif';
 
+const SLoadingContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  h1 {
+    font-size: 80px;
+    font-weight: 100;
+    background: linear-gradient(
+      to bottom,
+      #ffffff 0%,
+      #a0d7fc 40%,
+      #86c7f8 100%
+    );
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+  }
+`;
+
+const SImageContainer = styled.div`
+  width: 125px;
+  height: 125px;
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+`;
+
 const Loading: React.FC = () => {
   return (
-    <div style={{ display: 'flex' }}>
-      <img src={sunLogo} />
-      <h1 style={{ color: 'white' }}>Loading...</h1>;
-    </div>
+    <SLoadingContainer>
+      <SImageContainer>
+        <img src={sunLogo} />
+      </SImageContainer>
+
+      <div>
+        <h1 style={{ color: 'white' }}>APOD</h1>;
+      </div>
+    </SLoadingContainer>
   );
 };
 
